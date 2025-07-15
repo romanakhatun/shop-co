f54345ddf88c63eee6308fd677561e2c528ac5fc
romanakhatun <romanakhatun2004@gmail.com>
2025-07-15 23:47:01 +0600

    test practice

--- assets/main.js
+++ assets/main.js
@@ -2,4 +2,9 @@ jQuery(document).ready(function ($) {
   $(".cart-drawer-close").on("click", function () {
     $(".cart-drawer").addClass("hidden");
   });
+
+  $(".quick-view-btn").on("click", function () {
+    let productId = $(this).data("product-id");
+    // Open quick view modal for the selected product
+  });
 });
